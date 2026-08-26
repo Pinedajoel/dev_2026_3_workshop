62ec7da39888f9d352bbbf2a67ab5626b5966ba6
Pinedajoel <jspineda25@ucatolica.edu.co>
2026-08-25 21:26:44 -0500

    complete ta_te_ti_ganador

--- src/games/games.py
+++ src/games/games.py
@@ -63,7 +63,27 @@ class Games:
              ["O", "O", " "],
              [" ", " ", " "]] -> "X"
         """
-        pass
+
+        for fila in tablero:
+            if fila[0] == fila[1] == fila[2] != " ":
+                return "gano " + fila[0]
+  
+        for col in range(3):
+            if tablero[0][col] == tablero[1][col] == tablero[2][col] != " ":
+                return "gano " + tablero[0][col]
+        
+
+        if tablero[0][0] == tablero[1][1] == tablero[2][2] != " ":
+            return "gano " + tablero[0][0]
+        if tablero[0][2] == tablero[1][1] == tablero[2][0] != " ":
+            return "gano " + tablero[0][2]
+        
+
+        for fila in tablero:
+            if " " in fila:
+                return "continua"
+        
+        return "empate"
     
     def generar_combinacion_mastermind(self, longitud, colores_disponibles):
         """
