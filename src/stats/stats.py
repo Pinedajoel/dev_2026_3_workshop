af3b966d68e1165c5d8b82f1bea6e3687287e3b9
Pinedajoel <jspineda25@ucatolica.edu.co>
2026-08-30 21:33:33 -0500

    complete desviacion_standar

--- src/stats/stats.py
+++ src/stats/stats.py
@@ -78,7 +78,11 @@ class Stats:
         Ejemplo:
             desviacion_estandar([1, 2, 3, 4, 5]) -> 1.41...
         """
-        pass
+        if not numeros:
+            return 0
+        media = self.promedio(numeros)
+        varianza = sum((x - media) ** 2 for x in numeros) / len(numeros)
+        return varianza ** 0.5
     
     def varianza(self, numeros):
         """
